d16f1c3c5420483ac0843f697bb0254d104fc8b3
Shivam Wadhwa <shivamwadhwa7877@gmail.com>
2026-04-26 17:14:25 -0300

    Build: Game Completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,67 @@ import { useState } from 'react';
 import './App.css'
 import Cards from './Components/Cards';
 
+
+import aamir from "/src/assets/images/aamir.jpg";
+import akshay from "/src/assets/images/akshay.jpg";
+import bahubali from "/src/assets/images/bahubali.jpg";
+import bobby from "/src/assets/images/bobby.jpg";
+import kalki from "/src/assets/images/kalki.jpg";
+import krrish from "/src/assets/images/krrish.jpg";
+import pushpa from "/src/assets/images/pushpa.jpg";
+import ranbir from "/src/assets/images/ranbir.jpg";
+import ranveer from "/src/assets/images/ranveer.jpg";
+import rocky from "/src/assets/images/rocky.jpg";
+import salman from "/src/assets/images/salman.jpg";
+import sharukh from "/src/assets/images/sharukh.jpg";
+
+const initialCards = [
+    {id: 1, img: aamir, name: "aamir"},
+    {id: 2, img: akshay, name: "akshay"},
+    {id: 3, img: bahubali, name: "bahubali"},
+    {id: 4, img: bobby, name: "bobby"},
+    {id: 5, img: kalki, name: "kalki"},
+    {id: 6, img: krrish, name: "krrish"},
+    {id: 7, img: pushpa, name: "pushpa"},
+    {id: 8, img: ranbir, name: "ranbir"},
+    {id: 9, img: ranveer, name: "ranveer"},
+    {id: 10, img: rocky, name: "rocky"},
+    {id: 11, img: salman, name: "salman"},
+    {id: 12, img: sharukh, name: "sharukh"}
+]
+
 function App() {
 
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
   const [clickedCards, setClickedCards] = useState(new Set());
+  const [cards,setCards] = useState(initialCards);
+
+  function shuffleCards(){
+    const shuffled = [...cards].sort(() => Math.random() - 0.5);    
+    setCards(shuffled);
+  }
 
   function handleCardClick(id){
     if (clickedCards.has(id)){
-      setBestScore(score);
+      setBestScore((prev) => Math.max(prev, score));
       setScore(0);
       setClickedCards(new Set());
     }else{
       setScore((prev) => prev + 1);
       setClickedCards(prev => new Set(prev).add(id));
+      shuffleCards();
     }
   }
-
-
+  
   return(
   <div className='app'>
     <h1>Memory Card Game</h1>
+
     <p>Score: {score} | Best Score: {bestScore}</p>
-    <Cards onClick = {handleCardClick}/>
+    {score === 12 && <h2>You won</h2>}
+    <Cards cards = {cards} onClick = {handleCardClick}/>
+
   </div>
   )
 }
--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,40 +1,16 @@
-import aamir from "../assets/images/aamir.jpg";
-import akshay from "../assets/images/akshay.jpg";
-import bahubali from "../assets/images/bahubali.jpg";
-import bobby from "../assets/images/bobby.jpg";
-import kalki from "../assets/images/kalki.jpg";
-import krrish from "../assets/images/krrish.jpg";
-import pushpa from "../assets/images/pushpa.jpg";
-import ranbir from "../assets/images/ranbir.jpg";
-import ranveer from "../assets/images/ranveer.jpg";
-import rocky from "../assets/images/rocky.jpg";
-import salman from "../assets/images/salman.jpg";
-import sharukh from "../assets/images/sharukh.jpg";
+
 
 
 import "./cards.css";
 
 
-const cards = [
-    {id: 1, img: aamir, name: "aamir"},
-    {id: 2, img: akshay, name: "akshay"},
-    {id: 3, img: bahubali, name: "bahubali"},
-    {id: 4, img: bobby, name: "bobby"},
-    {id: 5, img: kalki, name: "kalki"},
-    {id: 6, img: krrish, name: "krrish"},
-    {id: 7, img: pushpa, name: "pushpa"},
-    {id: 8, img: ranbir, name: "ranbir"},
-    {id: 9, img: ranveer, name: "ranveer"},
-    {id: 10, img: rocky, name: "rocky"},
-    {id: 11, img: salman, name: "salman"},
-    {id: 12, img: sharukh, name: "sharukh"}
-]
 
-export default function Cards(){
+
+export default function Cards( {cards, onClick} ){
     return (
         <div className = "grid">
         {cards.map((card)=> (
-            <div key = {cards.id} className = "card">
+            <div key = {cards.id} className = "card" onClick={() => onClick(card.id)}>
                 <img src={card.img} alt={card.name} />
                 {/* <h1>{cards.name}</h1>  */}
                 {/* fix the above line of code as well not showing name on DOM  */}
